ab1875642a2caf63b9c7bd53f173b20eef21e904
zxm <617037386@qq.com>
2019-09-20 11:01:07 +0800

    修改了header的登录注册逻辑

--- footprint-client/src/components/sfq/Myheader.vue
+++ footprint-client/src/components/sfq/Myheader.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
+    <!-- logo  导航栏 -->
     <div class="header" :style="$store.getters.getThemeColor==0?'background-color: #202024;'
-      :$store.getters.getThemeColor==1?'background-color: #d60a16;'
+      :$store.getters.getThemeColor==1?'background-color: rgb(255, 3, 5);'
       :$store.getters.getThemeColor==2?'background-color: rgb(25, 143, 240);':''">
       <div class="headerC">
         <el-row :gutter="20">
@@ -44,18 +45,17 @@
               </div>
               <div class="headerR-text" v-if="showname">
                 <div>
-                  <el-avatar class="head-img" src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" >
+                  <el-avatar class="head-img" src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png">
                   </el-avatar>
-                  <!-- <span class="head-unum">欢迎{{form.unum}}!</span> -->
                   <el-dropdown :hide-on-click="false">
-                    <span class="el-dropdown-link" >
-                        欢迎{{form.unum}}!<i class="el-icon-arrow-down el-icon--right"></i>
+                    <span class="el-dropdown-link">
+                      欢迎{{username}}!<i class="el-icon-arrow-down el-icon--right"></i>
                     </span>
                     <el-dropdown-menu slot="dropdown">
                       <el-dropdown-item><span @click="persionalm">个人中心</span> </el-dropdown-item>
                       <el-dropdown-item>足迹管理</el-dropdown-item>
                       <el-dropdown-item>修改资料</el-dropdown-item>
-                      <el-dropdown-item>退出</el-dropdown-item>
+                      <el-dropdown-item><span @click="quit">退出</span></el-dropdown-item>
                     </el-dropdown-menu>
                   </el-dropdown>
                 </div>
@@ -67,7 +67,7 @@
       </div>
     </div>
     <!-- 登录弹框 -->
-    <el-dialog title="登录" :visible.sync="dialogFormVisible">
+    <el-dialog title="登录" :visible.sync="dialogFormVisible" :before-close="reset">
       <el-form>
         <!-- 账号 -->
         <el-form-item label="账号" :label-width="formLabelWidth">
@@ -75,24 +75,24 @@
           <el-input class="bgimg1" v-model="form.unum" autocomplete="off" @blur="blur" @focus="focus"
             placeholder="请输入您的账号"></el-input>
           <!-- 账号输入框下方内容 -->
-          <span
-            :class="unum_icon===0?'unull':unum_icon==1?'el-icon-circle-close':'el-icon-circle-check'">{{nameTxt}}</span>
+          <span class="ts" :class="login_unum==0?'active':''">用户名需为3~16为字母或数字的组合</span>
+          <span class="ts" :class="login_unum==1?'active el-icon-circle-close':''">用户名不能为空</span>
+          <span class="ts" :class="login_unum==2?'active el-icon-circle-check':''">用户名合法</span>
         </el-form-item>
         <!-- 密码 -->
         <el-form-item label="密码" :label-width="formLabelWidth">
           <!-- 密码输入框 -->
-          <el-input class="bgimg2" v-model="form.upwd" autocomplete="off" @blur="bull" @focus="focuu"
+          <el-input type="password" class="bgimg2" v-model="form.upwd" autocomplete="off" @blur="bull" @focus="focuu"
             placeholder="请输入您的密码">
-            <!-- <div><img src="../../assets/img/zxm/upwd.png" alt=""></div> -->
           </el-input>
           <!-- 密码输入框下方内容 -->
-          <span
-            :class="upwd_icon===0?'unull':upwd_icon==1?'el-icon-circle-close':'el-icon-circle-check'">{{upwdTxt}}</span>
+          <span class="ts" :class="login_upwd==0?'active':''">以字母开头，长度在6-18之间，只能包含字符、数字和下划线</span>
+          <span class="ts" :class="login_upwd==1?'active el-icon-circle-close':''">密码不能为空</span>
+          <span class="ts" :class="login_upwd==2?'active el-icon-circle-check':''">密码格式正确</span>
         </el-form-item>
       </el-form>
       <div class="icon-btn">
         <!-- 自动登录 -->
-        <!-- `checked` 为 true 或 false -->
         <el-checkbox>自动登录</el-checkbox>
         <!-- 忘记密码 -->
         <div>
@@ -101,25 +101,58 @@
       </div>
       <!-- 登录注册按钮 -->
       <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisibleregister=true,dialogFormVisible=false">去注册</el-button>
-        <el-button type="primary" @click="login">登录</el-button>
+        <el-button @click="dialogFormVisibleregister=true,dialogFormVisible=false,form2.unum1='',
+        form2.upwd1='',
+        form2.upwd2='',
+        form2.phone='',login_unum=-1,
+        login_upwd=-1,
+        reg_unum1=-1,
+        reg_upwd1=-1,
+        reg_phone=-1">去注册</el-button>
+        <el-button type="primary" @click="login" :disabled="login_unum==2&login_upwd==2?false:''">登录</el-button>
       </div>
     </el-dialog>
     <!-- 注册弹框 -->
-    <el-dialog title="注册" :visible.sync="dialogFormVisibleregister">
+    <el-dialog title="注册" :visible.sync="dialogFormVisibleregister" :before-close="reset">
       <el-form>
-        <el-form-item label="用户名称" :label-width="formLabelWidth">
-          <el-input v-model="form.unum" autocomplete="off" @blur="blur" @focus="focus"></el-input>
-          <span>{{nameTxt}}</span>
+        <el-form-item label="账号" :label-width="formLabelWidth">
+          <el-input placeholder="手机号码" v-model="form2.phone" @blur="blurp" @focus="focusp"></el-input>
+          <span class="ts" :class="reg_phone==0?'active':''">请输入您的手机号码</span>
+          <span class="ts" :class="reg_phone==1?'active el-icon-circle-close':''">请输入正确的手机号码</span>
+          <span class="ts" :class="reg_phone==2?'active el-icon-circle-check':''">手机号码格式正确</span>
         </el-form-item>
-        <el-form-item label="用户密码" :label-width="formLabelWidth">
-          <el-input v-model="form.upwd" autocomplete="off" @blur="bull" @focus="focuu"></el-input>
-          <span>{{upwdTxt}}</span>
+        <el-form-item label="昵称" :label-width="formLabelWidth">
+          <el-input v-model="form2.unum1" autocomplete="off" @blur="blurn" @focus="focusn"></el-input>
+          <span class="ts" :class="reg_unum1==0?'active':''">用户名需为3~16为字母或数字的组合</span>
+          <span class="ts" :class="reg_unum1==1?'active el-icon-circle-close':''">用户名不能为空</span>
+          <span class="ts" :class="reg_unum1==2?'active el-icon-circle-check':''">用户名合法</span>
+        </el-form-item>
+        <el-form-item label="密码" :label-width="formLabelWidth">
+          <el-input v-model="form2.upwd1" autocomplete="off" @blur="bullm" @focus="focuum"></el-input>
+          <!-- <span
+            :class="upwd1_icon===0?'unull':upwd1_icon==1?'el-icon-circle-close':'el-icon-circle-check'">{{upwdTxt1}}</span> -->
+          <span class="ts" :class="reg_upwd1==0?'active':''">以字母开头，长度在6-18之间，只能包含字符、数字和下划线</span>
+          <span class="ts" :class="reg_upwd1==1?'active el-icon-circle-close':''">密码不能为空</span>
+          <span class="ts" :class="reg_upwd1==2?'active el-icon-circle-check':''">密码格式正确</span>
+        </el-form-item>
+        <el-form-item label="密码确认" :label-width="formLabelWidth">
+          <el-input v-model="form2.upwd2" autocomplete="off" @blur="bagain" @focus="fagain"></el-input>
+          <span class="ts" :class="reg_upwd2==0?'active':''">请再次输入密码</span>
+          <span class="ts" :class="reg_upwd2==1?'active el-icon-circle-close':''">密码与第一次不符</span>
+          <span class="ts" :class="reg_upwd2==2?'active el-icon-circle-check':''">密码正确</span>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisibleregister=false,dialogFormVisible=true">去登录</el-button>
-        <el-button type="primary" @click="login">注册</el-button>
+        <el-button @click="dialogFormVisibleregister=false,dialogFormVisible=true,form.unum='',
+        form.upwd='',
+        login_unum=-1,
+        login_upwd=-1,
+        reg_unum1=-1,
+        reg_upwd1=-1,
+        reg_upwd2=-1,
+        reg_phone=-1">去登录</el-button>
+        <el-button type="primary" @click="register"
+          :disabled="reg_unum1==2&reg_upwd1==2&reg_phone==2&reg_upwd2==2?false:''">注册</el-button>
       </div>
     </el-dialog>
 
@@ -130,6 +163,8 @@
   var uPattern = /^[a-zA-Z0-9_-]{3,16}$/;
   // 密码强度正则，以字母开头，长度在6-18之间，只能包含字符、数字和下划线
   var pPattern = /^[a-zA-Z]\w{5,17}$/;
+  // 手机号正则验证表达式
+  var nPattern = /1[3-8]\d{9}/;
 
   export default {
     data() {
@@ -140,22 +175,54 @@
           unum: "",
           upwd: ""
         },
-        nameTxt: "",
-        upwdTxt: "",
+        form2: {
+          unum1: "",
+          upwd1: "",
+          upwd2: "",
+          phone: ""
+        },
+        login_unum: -1,
+        login_upwd: -1,
+        reg_unum1: -1,
+        reg_upwd1: -1,
+        reg_phone: -1,
+        reg_upwd2: -1,
         formLabelWidth: "120px",
         showname: false,
-        unum_icon: 0,
-        upwd_icon: 0,
+        username: ''
       };
     },
     methods: {
-      persionalm(){
+      reset(done) {
+        this.clear();
+        done();
+      },
+      quit() {
+        // location.reload();
+        this.showname = false;
+      },
+      clear() {
+        this.login_unum = -1;
+        this.login_upwd = -1;
+        this.reg_unum1 = -1;
+        this.reg_upwd1 = -1;
+        this.reg_phone = -1;
+        this.reg_upwd2 = -1;
+        this.form2.unum1 = "";
+        this.form2.upwd1 = "";
+        this.form2.upwd2 = "";
+        this.form2.phone = "";
+        this.form.unum = "";
+        this.form.upwd = "";
+      },
+      persionalm() {
         console.log("11111111");
         // this.$router.push("/home");
         this.$router.push("/personal");
       },
-      // ajax请求数据
+      // 登录ajax请求数据
       login() {
+
         this.axios.get("/login", {
           params: {
             unum: this.form.unum,
@@ -171,60 +238,168 @@
             });
             this.showname = true;
             this.dialogFormVisible = false;
+            this.username = this.form.unum;
+            this.clear();
+
           } else {
             //失败 提示
             this.$message.error("用户名或密码错误")
           }
         })
       },
+      // 注册-查询要插入的账号在数据库中是否存在
+      register() {
+        console.log(this.form2.phone)
+        this.axios.get("/login/register", {
+          params: {
+            uname: this.form2.unum1,
+            upwd: this.form2.upwd1,
+            unum: this.form2.phone
+          }
+        }).then(res => {
+          // 回调函数:接收服务器返回数据
+          if (res.data.code == -2) {
+            // 成功 
+            this.$message({
+              message: "请输入用户账号",
+            });
+          } else if (res.data.code == 0) {
+            this.$message({
+              message: "账号已存在",
+            });
+          } else if (res.data.code == 1) {
+            this.$message({
+              message: "注册成功"
+            });
+            this.dialogFormVisibleregister = false;
+            this.clear();
+          } else {
+            this.$message({
+              message: "注册失败",
+            });
+          }
+        })
+      },
+      //登录时用户名失去焦点时
       blur() {
         var name = this.form.unum;
         if (!name) {
-          this.nameTxt = "用户名不能为空";
-          this.unum_icon = 1;
+          this.login_unum = 1;
         } else if (!uPattern.test(name)) {
-          this.nameTxt = "用户名需为3~16为字母或数字的组合";
-          this.unum_icon = 1;
+          this.login_unum = 0;
         } else if (uPattern.test(name)) {
           this.nameTxt = "用户名合法";
-          this.unum_icon = 2;
-        } else {
-          this.unum_icon = 1;
+          this.login_unum = 2;
         }
       },
       focus() {
         var name = this.form.unum;
         if (!uPattern.test(name)) {
-          this.nameTxt = "用户名需为3~16为字母或数字的组合";
-          this.unum_icon = 1;
+          this.login_unum = 0;
         } else if (uPattern.test(name)) {
-          this.nameTxt = "用户名合法";
-          this.unum_icon = 2;
-        } else {
-          this.unum_icon = 1;
+          this.login_unum = 2;
         }
       },
+      // 登录密码失去焦点时
       bull() {
         var upwd = this.form.upwd;
         if (!upwd) {
-          this.upwdTxt = "密码不能为空";
-          this.upwd_icon = 1;
+          this.login_upwd = 1;
         } else if (!pPattern.test(upwd)) {
-          this.upwdTxt = "以字母开头，长度在6-18之间，只能包含字符、数字和下划线";
-          this.upwd_icon = 1;
+          this.login_upwd = 0;
         } else {
-          this.upwdTxt = "密码正确";
-          this.upwd_icon = 2;
+          this.login_upwd = 2;
         }
       },
+      // 登录密码获得焦点时
       focuu() {
         var upwd = this.form.upwd;
         if (!pPattern.test(upwd)) {
-          this.upwdTxt = "以字母开头，长度在6-18之间，只能包含字符、数字和下划线";
-          this.upwd_icon = 1;
+          this.login_upwd = 0;
         } else {
-          this.upwdTxt = "密码正确";
-          this.upwd_icon = 2;
+          this.login_upwd = 2;
+        }
+      },
+      blurp() {
+        var phone = this.form2.phone;
+        if (!phone) {
+          this.reg_phone = 0;
+        } else if (!nPattern.test(phone)) {
+          this.reg_phone = 1;
+        } else if (nPattern.test(phone)) {
+          this.reg_phone = 2;
+        }
+      },
+      // 注册时手机号获取焦点时
+      focusp() {
+        var phone = this.form2.phone;
+        if (!phone) {
+          this.reg_phone = 0;
+        } else if (!nPattern.test(phone)) {
+          this.reg_phone = 1;
+        } else if (nPattern.test(phone)) {
+          this.reg_phone = 2;
+        }
+      },
+      // 注册时昵称获得焦点时
+      focusn() {
+        var name1 = this.form2.unum1;
+        if (!uPattern.test(name1)) {
+          this.reg_unum1 = 0;
+        } else if (uPattern.test(name1)) {
+          this.reg_unum1 = 2;
+        }
+      },
+      // 注册时昵称失去焦点时
+      blurn() {
+        var name1 = this.form2.unum1;
+        if (!name1) {
+          this.reg_unum1 = 1;
+        } else if (!uPattern.test(name1)) {
+          this.reg_unum1 = 0;
+        } else if (uPattern.test(name1)) {
+          this.reg_unum1 = 2;
+        }
+      },
+      // 注册时密码失去焦点时
+      bullm() {
+        var upwd1 = this.form2.upwd1;
+        if (!upwd1) {
+          this.reg_upwd1 = 1;
+        } else if (!pPattern.test(upwd1)) {
+          this.reg_upwd1 = 0;
+        } else {
+          this.reg_upwd1 = 2;
+        }
+      },
+      // 注册时密码获得焦点时
+      focuum() {
+        var upwd1 = this.form2.upwd1;
+        if (!pPattern.test(upwd1)) {
+          this.reg_upwd1 = 0;
+        } else {
+          this.reg_upwd1 = 2;
+        }
+      },
+      // 二次确认密码失去焦点时
+      bagain() {
+        var upwd2 = this.form2.upwd2;
+        if (!upwd2) {
+          this.reg_upwd2 = 1;
+        } else if (this.form2.upwd2 == this.form2.upwd1) {
+
+          this.reg_upwd2 = 2;
+        } else {
+          this.reg_upwd2 = 1;
+        }
+      },
+      // 二次确认密码获得焦点时
+      fagain() {
+        var upwd1 = this.form2.upwd1;
+        if (!pPattern.test(upwd1)) {
+          this.reg_upwd1 = 0;
+        } else {
+          this.reg_upwd1 = 2;
         }
       },
     },
@@ -232,6 +407,14 @@
 </script>
 
 <style>
+  .ts {
+    display: none;
+  }
+
+  .ts.active {
+    display: block;
+  }
+
   .header .headerC .headerL img {
     width: 60px;
     height: 60px;
@@ -269,7 +452,6 @@
   }
 
   .header .headerC .headerM ul {
-    /* display: flex; */
     margin: 0;
     list-style: none;
   }
@@ -295,13 +477,16 @@
     color: white;
     padding-left: 10px
   }
+
   .el-dropdown-link {
     cursor: pointer;
     color: #409EFF;
   }
+
   .el-icon-arrow-down {
     font-size: 12px;
   }
+
   .header .headerC .headerR .headerR-text .head-img {
     width: 40px;
     height: 40px;
@@ -313,7 +498,6 @@
     width: 50%;
     border-bottom: 1px solid #fff;
     padding: 0 10px;
-
   }
 
   .headerR-text button {
@@ -322,13 +506,11 @@
     margin-top: 5px;
     padding: 6px 10px;
     vertical-align: bottom;
-
   }
 
   .header .headerC .headerR input {
     width: 80%;
     height: 30px;
-    /* border-radius: 25px; */
     border: 0;
     outline: 0;
     background-color: transparent;
@@ -348,8 +530,6 @@
 
   .header .headerC .headerR .el-button--text {
     color: #fff !important;
-    /* float: right; */
-    /* margin: -60px 100px 0 0 !important; */
   }
 
   .el-input__inner {
